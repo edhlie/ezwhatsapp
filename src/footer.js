f35a2286327afd161c2bed3b74ca3414c606dc93
Edward Lie <edlie@Who-Dey.local>
2022-08-11 14:42:09 +0700

    add separate footer file

---
+++ src/footer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  Avatar,
+  Link,
+  Typography,
+  Grid,
+} from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+// import {
+//   GitHubIcon,
+//   LinkedInIcon,
+// } from '@mui/icons-material';
+
+
+export default function Copyright(props) {
+  return (
+    <footer>
+      <Typography variant='body2' color='text.secondary' align='center' {...props}>
+        <Link color='inherit' target='_blank' rel='noopener' href='https://linkedin.com/in/edwardhlie'>
+          <LinkedInIcon sx={{m:1, fontSize:'35px'}}/>
+        </Link>
+        <Link color='inherit' target='_blank' rel='noopener' href='https://github.com/edhlie'>
+          <GitHubIcon sx={{m:1, fontSize:'35px'}}/>
+        </Link>
+      </Typography>
+    </footer>
+  );
+}
